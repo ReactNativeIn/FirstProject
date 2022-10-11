b5960100c44b480d9fd012ac049f1e2ef7535b02
ddcc <chdys258@gmail.com>
2022-10-11 16:54:49 +0900

    init

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>test111</Text>
+      <Text>test3</Text>
       <StatusBar style="auto" />
     </View>
   );
