efdbd9ea4c2f4a21c5ea79284139901d701835ed
ddcc <chdys258@gmail.com>
2022-10-17 16:01:36 +0900

    다른사람 프로필

--- screens/ProfileStack.js
+++ screens/ProfileStack.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import ProfileTab from './ProfileTab';
+// import UserProfileTab from './UserProfileTab';
 import MessageScreen from './MessageScreen';
 import EditProfile from './EditProfile';
 
